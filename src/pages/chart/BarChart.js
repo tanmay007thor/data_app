acca9d7d914748750527e92e9e5ef6d63e7a8331
tanmayrathod777 <tanmay.rathod@digiqt.com>
2023-02-22 13:05:15 +0530

    new_commit

--- src/pages/chart/BarChart.js
+++ src/pages/chart/BarChart.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
+import axios from 'axios';
+const url = `https://data-analysis-85r1.onrender.com/api/v1/energy/`;
+
 import { CChart } from '@coreui/react-chartjs'
 
 export default function BarChart() {
+  const [data, setData] = useState();
+  const [country, setCountry] = useState([]);
+  const [intensityData, setIntensityData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response?.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      const countries = Object.values(data).map((obj) => obj.sector);
+      const intensities = Object.values(data).map((obj) => obj.intensity);
+      setCountry(countries);
+      setIntensityData(intensities);
+    }
+  }, [data]);
+
   return (
     <div>
         <CChart
   type="bar"
   data={{
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: country,
     datasets: [
       {
         label: 'GitHub Commits',
-        backgroundColor: '#f87979',
-        data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
+        backgroundColor: '#f56978',
+        data: intensityData,
       },
     ],
   }}
--- src/pages/chart/Chart.js
+++ src/pages/chart/Chart.js
@@ -4,63 +4,46 @@ const url = `https://data-analysis-85r1.onrender.com/api/v1/energy/`;
 import axios from "axios";
 console.log(url);
 export default function Chartis() {
-  const [data, setData] = useState({});
-  const [yearIntensityData, setYearIntensityData] = useState([]);
-  const [sector, setSector] = useState([]);
+  const [data, setData] = useState();
+  const [country, setCountry] = useState([]);
+  const [intensityData, setIntensityData] = useState([]);
 
   useEffect(() => {
     axios
       .get(url)
       .then((response) => {
         setData(response?.data);
-        // console.log(response?.data)
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
+
   useEffect(() => {
-    if (data && data.length > 0) {
-      const intensityData = data.map((item) => (
-        item.sector
-      ));
-      setSector(sector);
+    if (data) {
+      const countries = Object.values(data).map((obj) => obj.sector);
+      const intensities = Object.values(data).map((obj) => obj.intensity);
+      setCountry(countries);
+      setIntensityData(intensities);
     }
   }, [data]);
-  useEffect(() => {
-    if (data && data.length > 0) {
-      const intensityData = data.map((item) => (
-        item.intensity
-      ));
-      setYearIntensityData(intensityData);
-    }
-  }, [data]);
-  
-  console.log(yearIntensityData)
+
 
   return (
     <div>
       <CChart
         type="line"
         data={{
-          labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-          ],
+          labels: country,
           datasets: [
             {
               label: "My First dataset",
-              backgroundColor: "rgba(220, 220, 220, 0.2)",
+              backgroundColor: "rgba(111, 317, 220, 0.2)",
               borderColor: "rgba(110, 110, 110, 1)",
               pointBackgroundColor: "rgba(220, 220, 220, 1)",
               pointBorderColor: "#fff",
-              data: yearIntensityData,
-            }
+              data: intensityData,
+            },
           ],
         }}
       />
